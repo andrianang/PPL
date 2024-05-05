0b9014984af3ee01193126183666bbc02dd616ed
andrianang <andrianangg@gmail.com>
2024-05-05 23:47:33 +0700

    fix report

--- report.py
+++
@@ -1,8 +0,0 @@
-class Report:
-    def __init__(self, coffee_maker, money_machine):
-        self.coffee_maker = coffee_maker
-        self.money_machine = money_machine
-
-    def generate_report(self):
-        self.coffee_maker.report()
-        self.money_machine.report()
