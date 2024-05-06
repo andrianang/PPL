a5d17d34efb3360b2d60fb249f400d870f553045
yoel-mc <yoel@mulaicoding.id>
2024-05-06 15:53:30 +0700

    feat: Complete DRY Principle

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from menu import Menu
 from transaction_manager import TransactionManager
 
-menu = Menu()
 transaction_manager = TransactionManager()
+menu = transaction_manager.menu
 
 while True:
-    options = menu.get_items()
-    user_choice = input(f"What would you like? ({options}): ").lower()
+    user_choice = input(f"What would you like? ({menu.get_items()}): ").lower()
     if user_choice == "report":
         transaction_manager.report_generate()
     elif user_choice == "off":
